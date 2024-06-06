e00e49ea69961ef49dae7e771eb6e02ebcce5109
Pedro Augusto Borges dos Santos <pabsantos21@gmail.com>
2024-06-06 09:37:08 -0300

    Create new join and download functions

--- R/nds_download_sf.R
+++ R/nds_download_sf.R
@@ -20,3 +20,26 @@ nds_download_sf <- function(url) {
   unlink(c(temp, temp2))
   return(file)
 }
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nds_download_cwb_osm <- function() {
+  message("Downloading OSM data...")
+  cwb_bbox <- osmdata::getbb("Curitiba, Brazil")
+  values <- c(
+    "motorway", "trunk", "primary", "secondary", "tertiary", "road",
+    "residential", "motorway_link", "trunk_link", "primary_link",
+    "secondary_link", "tertiary_link"
+  )
+  osm_cwb <- osmdata::opq(bbox = cwb_bbox) |>
+    osmdata::add_osm_feature(key = "highway", value = values) |>
+    osmdata::osmdata_sf()
+  cwb_axis <- osm_cwb$osm_lines
+  selected_vars <- c("osm_id", "name", "highway", "maxspeed")
+  cwb_axis <- subset(cwb_axis, select = selected_vars)
+  return(cwb_axis)
+}
--- R/nds_join.R
+++ R/nds_join.R
@@ -1,14 +1,33 @@
+#' Title
+#'
+#' @param ndsbr_data
+#' @param road_axis
+#' @param axis_vars
+#'
+#' @return
+#' @export
+#'
+#' @examples
 nds_join_axis <- function(
     ndsbr_data,
     road_axis,
     axis_vars = c("NMVIA", "SVIARIO", "HIERARQUIA")) {
+  message("Making a 10-meter buffer around axis data...")
+
   road_axis_buffer <- sf::st_buffer(road_axis, dist = 10, endCapStyle = "FLAT")
+
   if (sf::st_crs(road_axis_buffer)$input != "EPSG:4674") {
     road_axis_buffer <- sf::st_transform(road_axis_buffer, 4674)
   }
 
   road_axis_buffer <- subset(road_axis_buffer, select = axis_vars)
+
+  message("Joining data...")
+
   nds_joined_data <- sf::st_join(ndsbr_data, road_axis_buffer)
+
+  message("Arranging names and road hierarchy data...")
+
   nds_joined_data <- dplyr::distinct(
     nds_joined_data,
     .data$ID,
@@ -32,6 +51,16 @@ nds_join_axis <- function(
   return(nds_joined_data)
 }
 
+#' Title
+#'
+#' @param ndsbr_data
+#' @param neigh_data
+#' @param vars
+#'
+#' @return
+#' @export
+#'
+#' @examples
 nds_join_neigh <- function(ndsbr_data, neigh_data, vars = "NOME") {
   if (sf::st_crs(neigh_data)$input != "EPSG:4674") {
     neigh_data <- sf::st_transform(neigh_data, 4674)
@@ -43,5 +72,69 @@ nds_join_neigh <- function(ndsbr_data, neigh_data, vars = "NOME") {
   nds_joined_data$NOME <- NULL
   nds_joined_data$BAIRRO[is.na(nds_joined_data$BAIRRO)] <- "NPI"
   nds_joined_data$CIDADE[nds_joined_data$BAIRRO != "NPI"] <- "Curitiba"
+  return(nds_joined_data)
+}
+
+#' Title
+#'
+#' @param ndsbr_data
+#' @param osm_data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+nds_join_spdlimit <- function(ndsbr_data, osm_data, vars = "maxspeed") {
+
+  message("Making a 10-meter buffer around axis data...")
+
+  osm_data <- subset(osm_data, select = vars)
+  osm_data_buffer <- sf::st_buffer(osm_data, dist = 10, endCapStyle = "FLAT")
+
+  message("Joining data...")
+
+  if (sf::st_crs(osm_data_buffer)$input != "EPSG:4674") {
+    osm_data_buffer <- sf::st_transform(osm_data_buffer, 4674)
+  }
+
+  nds_joined_data <- sf::st_join(ndsbr_data, osm_data_buffer)
+
+  message("Arranging speed limit data...")
+
+  nds_joined_data <- dplyr::distinct(
+    nds_joined_data,
+    .data$ID,
+    .data$TIME_ACUM,
+    .keep_all = TRUE
+  )
+
+  nds_joined_data$LIMITE_VEL <- nds_joined_data$maxspeed
+  nds_joined_data$maxspeed <- NULL
+  nds_joined_data$LIMITE_VEL[is.na(nds_joined_data$LIMITE_VEL)] <- "NPI"
+
+
+  ## Corrigir essa parte
+  nds_joined_data$LIMITE_VEL <- ifelse(
+    nds_joined_data$LIMITE_VEL == "NPI",
+    ifelse(
+      nds_joined_data$HIERARQUIA_CTB == "1",
+      "80",
+      ifelse(
+        nds_joined_data$HIERARQUIA_CTB == "2",
+        "60",
+        ifelse(
+          nds_joined_data$HIERARQUIA_CTB == "3",
+          "40",
+          ifelse(
+            nds_joined_data$HIERARQUIA_CTB == "4",
+            "30",
+            "NPI"
+          )
+        )
+      )
+    ),
+    nds_joined_data$LIMITE_VEL
+  )
+
   return(nds_joined_data)
 }
